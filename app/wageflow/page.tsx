ef43b72ae1404db534437801480b80f8785c3126
Adam Manley <adammanley73@gmail.com>
2026-01-09 22:12:49 +0000

    Fix/wageflow marketing route (#23)
    * Add host-based routing for www.thebusinessconsortiumltd.co.uk
    
    * Expose WageFlow marketing page at /wageflow
    
    ---------
    
    Co-authored-by: Adam Manley <thebusinessconsortium@outlook.com>

---
+++ app/wageflow/page.tsx
@@ -0,0 +1,8 @@
+// C:\Users\adamm\Projects\wageflow01\app\wageflow\page.tsx
+
+import React from "react";
+import WageFlowLandingV2 from "@/components/marketing/landings/WageFlowLanding_V2";
+
+export default function Page() {
+  return <WageFlowLandingV2 />;
+}
