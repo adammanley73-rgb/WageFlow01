1165d5f771968705aac3eeed47d5ccf9fb9844ea
Adam Manley <adammanley73@gmail.com>
2026-02-25 20:37:01 +0000

    Feat/bereaved partners paternity 2026 (#85)
    * fix(absence): align absences.type values with DB constraint
    
    * db(absences): allow bereaved_partners_paternity type

---
+++ supabase/migrations/20260225195500_absences_add_bereaved_partners_paternity.sql
@@ -0,0 +1,25 @@
+﻿-- C:\Projects\wageflow01\supabase\migrations\20260225195500_absences_add_bereaved_partners_paternity.sql
+
+begin;
+
+alter table public.absences
+  drop constraint if exists absences_type_check;
+
+alter table public.absences
+  add constraint absences_type_check
+  check (
+    (type is null)
+    or (type = any (array[
+      'annual_leave'::text,
+      'sickness'::text,
+      'maternity'::text,
+      'paternity'::text,
+      'shared_parental'::text,
+      'adoption'::text,
+      'parental_bereavement'::text,
+      'unpaid_leave'::text,
+      'bereaved_partners_paternity'::text
+    ]))
+  );
+
+commit;
